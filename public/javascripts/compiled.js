f5fbde4618369f55f1640fbb387acddb08f84538
Steve Romain <smromain@gmail.com>
2015-03-16 13:58:40 -0400

    removed console.log from artist card

--- public/javascripts/compiled.js
+++ public/javascripts/compiled.js
@@ -36,7 +36,6 @@ var Card = React.createClass({displayName: "Card",
                 function(data){
                     var bio = data.bio;
                     var item = data.tracks;
-                    console.log(item);
                     var name = item[0].artists[0].name;
                     var ids = [];
                     item.forEach(function(item){
