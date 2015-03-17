f0b8e51147b7a568626aa7f109b1eb2f97159169
Joseph Alves <databentpx@gmail.com>
2015-03-16 23:02:14 -0400

    Updated gitignore and put in a gulp process

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var path = require('path'),
+    gulp = require('gulp'),
+    browserify = require('browserify'),
+    reactify = require('reactify'),
+    source = require('vinyl-source-stream'),
+    plumber = require('gulp-plumber');
+
+var MAIN_JS_FILE = path.join(__dirname, './client/javascripts/main.js');
+var PUBLIC_JS_DIR = path.join(__dirname, './public/javascripts');
+
+gulp.task('compileJS', function () {
+
+    var b = browserify({
+        insertGlobals: true
+    }).add(MAIN_JS_FILE).transform(reactify);
+
+    var bundling = b.bundle();
+
+    bundling.on('error', function (err) {
+        console.error(err.message);
+        bundling.emit('end');
+    });
+
+    return bundling
+        .pipe(plumber())
+        .pipe(source('compiled.js'))
+        .pipe(gulp.dest(PUBLIC_JS_DIR));
+
+});
+
+gulp.task('default', function () {
+    gulp.watch('client/javascripts/**/*.js', ['compileJS']);
+});
