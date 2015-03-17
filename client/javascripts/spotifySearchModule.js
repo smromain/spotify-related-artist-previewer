a0217732468c8cd62023491d42bc99fca9236ce8
Joseph Alves <databentpx@gmail.com>
2015-03-17 00:11:22 -0400

    Refactoring spotify search

--- client/javascripts/spotifySearchModule.js
+++ client/javascripts/spotifySearchModule.js
@@ -1,56 +1,135 @@
 var $ = require('jQuery');
 var Promise = require("bluebird");
 
-function artistSearch(query, cb){
-	$.ajax({
-	    url: 'https://api.spotify.com/v1/search',
-	    data: {
-	        q: query,
-	        type: 'artist'
-	    },
-	    success: function (origResp) {
-        	$.ajax({
-				url: 'https://api.spotify.com/v1/artists/' + origResp.artists.items[0].id + '/related-artists',
-				success: function(data){
-					return data.artists;
-				}
-			})
-			.done(
-				function(data){
-
-					var finalDataPromises = [];
-
-					data.artists.forEach(function(item){
-						finalDataPromises.push(new Promise(function(resolve,reject){
-							$.ajax({
-								url: 'https://api.spotify.com/v1/artists/' + item.id + '/top-tracks?country=us',
-								success: function(data){
-									$.ajax({
-										url: 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + item.name + '&api_key=4ab8061faad02381db25568da3d061ff&format=json',
-										success: function(lastfm){
-											data.bio = $('<div>' + lastfm.artist.bio.summary + '</div>').text().split('Read more about')[0];
-											return resolve(data);
-										}
-									})
-								}
-							})
-						}))
-
-					})
-
-					Promise.settle(finalDataPromises).then(function(results){
-						var resultingData = [];
-						results.forEach(function(item){
-							resultingData.push({tracks: item.value().tracks, bio: item.value().bio});
-						})
-						return cb(resultingData);
-					})
-
-				}
-
-			)
-	    }
-	})
+/**
+ * jQuery trick for sanitizing string.
+ * @param html
+ * @returns New string after sanitization.
+ */
+var sanitizeHTML = function (html) {
+    return $('<div>' + html + '</div>').text();
 };
 
-module.exports = {relatedSearch: artistSearch};
+/**
+ * Make AJAX request to Spotify for artist information.
+ * @param artist
+ * @returns {bluebird}
+ * @resolves artist object.
+ */
+var searchForArtist = function (artist) {
+
+    return new Promise(function (resolve, reject) {
+
+        $.ajax({
+            url: 'https://api.spotify.com/v1/search',
+            data: {
+                q: artist,
+                type: 'artist'
+            },
+            success: function (response) {
+                resolve(response.artists.items[0]);
+            },
+            error: function (err) {
+                reject(err);
+            }
+        });
+
+    });
+
+};
+
+/**
+ * Finds the artists related to base artist.
+ * @param baseArtist
+ * @returns {bluebird}
+ * @resolves Array of artist objects.
+ */
+var getRelatedArtists = function (baseArtist) {
+
+    return new Promise(function (resolve, reject) {
+
+        $.ajax({
+            url: 'https://api.spotify.com/v1/artists/' + baseArtist.id + '/related-artists',
+            success: function (response) {
+                resolve(response.artists);
+            },
+            error: function (err) {
+                reject(err);
+            }
+        });
+
+    });
+
+};
+
+/**
+ * Retrieves the tracks for a given artist.
+ * @param artist
+ * @returns {bluebird}
+ * @resolves Object with an array on key `tracks`.
+ */
+var getArtistTracks = function (artist) {
+
+    return new Promise(function (resolve, reject) {
+
+        $.ajax({
+            url: 'https://api.spotify.com/v1/artists/' + artist.id + '/top-tracks?country=us',
+            success: function (response) {
+                resolve(response);
+            },
+            error: function (err) {
+                reject(err);
+            }
+        });
+
+    });
+
+};
+
+/**
+ * Retrieves summary of artist.
+ * @param artist
+ * @returns {bluebird}
+ * @resolves Sanitized string.
+ */
+var getArtistBioFromLastFM = function (artist) {
+
+    return new Promise(function (resolve, reject) {
+
+        $.ajax({
+            url: 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + artist.name + '&api_key=4ab8061faad02381db25568da3d061ff&format=json',
+            success: function (response) {
+                var summary = response.artist.bio.summary;
+                summary = sanitizeHTML(summary).split('Read more about')[0];
+                resolve(summary);
+            },
+            error: function (err) {
+                reject(err);
+            }
+        });
+
+    });
+
+};
+
+/**
+ * Gets related artist information by composing tracks and bio retrievals.
+ * @param artist
+ * @returns {*}
+ */
+var getRelatedArtistInfo = function (artist) {
+
+    var promises = [getArtistTracks(artist), getArtistBioFromLastFM(artist)];
+
+    return Promise.all(promises).spread(function (artistWithTracks, bio) {
+        artistWithTracks.bio = bio;
+        return artistWithTracks;
+    });
+
+};
+
+module.exports = {
+    relatedSearch: function (query, cb) {
+        return searchForArtist(query).then(getRelatedArtists).map(getRelatedArtistInfo);
+    }
+};
